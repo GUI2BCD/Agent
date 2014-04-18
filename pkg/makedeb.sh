9a8d67f85166b8a2ec46bddf08132d6514ed42a8
Cameron Morris <zephyrz2712@gmail.com>
2014-04-18 18:55:58 -0400

    Added deb files

---
+++ pkg/makedeb.sh
@@ -0,0 +1,8 @@
+mkdir lastresortrecovery_0.2-beta
+cp -r DEBIAN lastresortrecovery_0.2-beta
+git clone http://github.com/GUI2BCD/Agent.git
+cd Agent/src
+make DESTDIR="../../lastresortrecovery_0.2-beta/" install
+cd ../..
+dpkg-deb --build lastresortrecovery_0.2-beta
+rm -rf Agent
